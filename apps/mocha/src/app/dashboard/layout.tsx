66db0d78df5e632484f15a93c78433f4ccf05822
Ericson Funtanar <ericsonfuntanar@Ericsons-MacBook-Air.local>
2024-02-26 00:17:16 +0800

    x

--- apps/mocha/src/app/dashboard/layout.tsx
+++ apps/mocha/src/app/dashboard/layout.tsx
@@ -18,9 +18,9 @@ export default async function DashboardLayout({
 }) {
   const session = await getServerSession(authOption);
   const user = await getUser(session?.user?.email ?? "");
-  if (user.email) {
-    redirect(`/auth/login`);
-  }
+  // if (user.email) {
+  //   redirect(`/auth/login`);
+  // }
   return (
     <div>
       <Nav />
