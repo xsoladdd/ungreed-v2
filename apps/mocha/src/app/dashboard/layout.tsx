1824559cb8e3fb864a0118c272191c40bbcc7a17
Ericson Funtanar <ericsonfuntanar@Ericsons-MacBook-Air.local>
2024-02-26 00:41:49 +0800

    x

--- apps/mocha/src/app/dashboard/layout.tsx
+++ apps/mocha/src/app/dashboard/layout.tsx
@@ -25,6 +25,7 @@ export default async function DashboardLayout({
   return (
     <div>
       <Nav />
+      {JSON.stringify(session)}
       {/* <DashboardProvider session={session} user={user}> */}
       {children}
       {/* </DashboardProvider> */}
