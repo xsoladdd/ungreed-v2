d9703544eca7aec3fb41c0ed8d0e9d42648513b2
Ericson Funtanar <ericsonfuntanar@Ericsons-MacBook-Air.local>
2024-02-26 00:39:11 +0800

    x

--- apps/mocha/src/app/dashboard/layout.tsx
+++ apps/mocha/src/app/dashboard/layout.tsx
@@ -2,9 +2,9 @@ import Nav from "@/components/layout/navbar/nav";
 import type { Metadata } from "next";
 // import DashboardProvider from "@/components/providers/DashboardProvider";
 // import { getUser } from "@/lib/getUser";
-// import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth";
 // import { redirect } from "next/navigation";
-// import { authOption } from "../api/auth/[...nextauth]/helper";
+import { authOption } from "../api/auth/[...nextauth]/helper";
 
 export const metadata: Metadata = {
   title: "UNGREED | DASHBOARD",
@@ -16,11 +16,12 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await getServerSession(authOption);
+  const session = await getServerSession(authOption);
   // const user = await getUser(session?.user?.email ?? "");
   // if (user.email) {
   //   redirect(`/auth/login`);
   // }
+  console.log(session);
   return (
     <div>
       <Nav />
