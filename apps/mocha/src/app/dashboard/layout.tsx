4bad2dc92970ed718223a1cdd973b15f14172d71
Ericson Funtanar <ericsonfuntanar@Ericsons-MacBook-Air.local>
2024-02-26 00:14:26 +0800

    x

--- apps/mocha/src/app/dashboard/layout.tsx
+++ apps/mocha/src/app/dashboard/layout.tsx
@@ -1,10 +1,10 @@
 import Nav from "@/components/layout/navbar/nav";
 import type { Metadata } from "next";
-// import DashboardProvider from "@/components/providers/DashboardProvider";
-// import { getUser } from "@/lib/getUser";
-// import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
-// import { authOption } from "../api/auth/[...nextauth]/helper";
+import DashboardProvider from "@/components/providers/DashboardProvider";
+import { getUser } from "@/lib/getUser";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOption } from "../api/auth/[...nextauth]/helper";
 
 export const metadata: Metadata = {
   title: "UNGREED | DASHBOARD",
@@ -16,17 +16,17 @@ export default async function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  // const session = await getServerSession(authOption);
-  // const user = await getUser(session?.user?.email ?? "");
-  // if (user.email) {
-  //   redirect(`/auth/login`);
-  // }
+  const session = await getServerSession(authOption);
+  const user = await getUser(session?.user?.email ?? "");
+  if (user.email) {
+    redirect(`/auth/login`);
+  }
   return (
     <div>
       <Nav />
-      {/* <DashboardProvider session={session} user={user}> */}
-      {children}
-      {/* </DashboardProvider> */}
+      <DashboardProvider session={session} user={user}>
+        {children}
+      </DashboardProvider>
     </div>
   );
 }
