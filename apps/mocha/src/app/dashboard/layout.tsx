6322ceaa13f99c60ddfd03261e640c888b58eb02
Ericson Funtanar <ericsonfuntanar@Ericsons-MacBook-Air.local>
2024-02-26 00:52:57 +0800

    x

--- apps/mocha/src/app/dashboard/layout.tsx
+++ apps/mocha/src/app/dashboard/layout.tsx
@@ -1,11 +1,10 @@
 import Nav from "@/components/layout/navbar/nav";
 import type { Metadata } from "next";
-// import DashboardProvider from "@/components/providers/DashboardProvider";
-// import { getUser } from "@/lib/getUser";
+import DashboardProvider from "@/components/providers/DashboardProvider";
+import { getUser } from "@/lib/getUser";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOption } from "../api/auth/[...nextauth]/helper";
-import { getUser } from "@/lib/getUser";
 
 export const metadata: Metadata = {
   title: "UNGREED | DASHBOARD",
@@ -18,18 +17,18 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const session = await getServerSession(authOption);
-  // const user = await getUser(session?.user?.email ?? "");
   if (!session) {
     redirect(`/auth/login`);
   }
+  const user = await getUser(session?.user?.email ?? "");
   // console.log(session);
   return (
     <div>
       <Nav />
       {JSON.stringify(session)}
-      {/* <DashboardProvider session={session} user={user}> */}
-      {children}
-      {/* </DashboardProvider> */}
+      <DashboardProvider session={session} user={user}>
+        {children}
+      </DashboardProvider>
     </div>
   );
 }
